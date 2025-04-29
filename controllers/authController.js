d92574665121e2f3154266eff87bc14e1f488a72
Eduardo Sampaio <duardosampaiio456@gmail.com>
2025-04-29 09:07:47 -0300

    feat: iniciando o server

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const User = require('../models/User.');
+const User = require("../models/user");
 
 exports.register = async (req, res) => {
   try {
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
+// imports
 require('dotenv').config();
 const express = require('express');
 const http = require('http');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const socketIo = require('socket.io');
-const authRoutes = require('./routes/authRoutes');
+const authRoutes = require('./routes/authRoutes'); // Importa as rotas de autenticação
 const socketHandler = require('./socket/socketHandler');
 
 const app = express();
@@ -13,12 +14,20 @@ const io = socketIo(server, { cors: { origin: '*' } });
 
 app.use(cors());
 app.use(express.json());
+
+// Usando as rotas de autenticação
 app.use('/api/auth', authRoutes);
 
+// Rota raiz personalizada
+app.get('/', (req, res) => {
+    res.send("Bem-vindo ao server!");
+});
+
+// Conectando ao Banco de Dados
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('MongoDB conectado');
-    socketHandler(io);
+    socketHandler(io); // Configura o socket
     server.listen(process.env.PORT, () => {
       console.log(`Servidor rodando na porta ${process.env.PORT}`);
     });
