d09b4616fe6ae4f619c75bf6428d8aacbf90b22b
Eduardo Sampaio <duardosampaiio456@gmail.com>
2025-04-29 09:27:21 -0300

    feat: configurando autorização de login

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,14 +1,14 @@
 const jwt = require('jsonwebtoken');
 
-module.exports = function (req, res, next) {
-  const token = req.headers.authorization?.split(' ')[1];
-  if (!token) return res.status(401).send({ error: 'Token ausente' });
+module.exports = (req, res, next) => {
+  const token = req.cookies?.token || req.headers.authorization?.split(" ")[1];
+  if (!token) return res.redirect('/');
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
     next();
   } catch (err) {
-    res.status(401).send({ error: 'Token inválido' });
+    res.redirect('/');
   }
 };
