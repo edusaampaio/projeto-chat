cd83a0b4c49b87dbc3db64d32d735ccbb49d21cc
Eduardo Sampaio <duardosampaiio456@gmail.com>
2025-04-29 09:37:48 -0300

    feat: criando nova rota de registro

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController');
+const { register, login } = require('../controllers/authController');
 
-router.post('/register', authController.register);
-router.post('/login', authController.login);
+const router = express.Router();
+
+router.get('/register', (req, res) => res.render('register'));
+router.post('/register', register);
+router.post('/login', login);
 
 module.exports = router;
+
