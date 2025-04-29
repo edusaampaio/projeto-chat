abe3f0f90c5befa713c9935c2a9ab5c1fc920547
Eduardo Sampaio <duardosampaiio456@gmail.com>
2025-04-29 09:32:21 -0300

    feat: passando o tipo de arquivo que vai ser enviado

--- public/js/chat.js
+++ public/js/chat.js
@@ -5,3 +5,9 @@ function sendMessage() {
   socket.emit('chatMessage', input.value);
   input.value = '';
 }
+socket.on('chatMessage', msg => {
+    const div = document.getElementById('messages');
+    const p = document.createElement('p');
+    p.innerText = msg;
+    div.appendChild(p);
+  });
